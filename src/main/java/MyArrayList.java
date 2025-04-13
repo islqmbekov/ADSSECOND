4d816f9a78c52d25b5f0265fdca6ccd7f000fd61
Armat <armat.islambekovv@gmail.com>
2025-04-13 21:03:12 +0500

    MYSTACK

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -1,69 +1,54 @@
 // ==== MyArrayList START ====
 
-import java.util.Iterator;
-
 /**
- * MyArrayList is a custom implementation of a dynamic array list.
- * This class implements MyList<T> interface and uses an Object[] array internally.
- * It provides methods to add, remove, and access elements, similar to Java's ArrayList.
+ * MyArrayList is a custom generic dynamic array implementation.
+ * It supports basic operations such as add, get, set, remove, size, and clear.
  */
 
-public class MyArrayList<T extends Comparable<T>> implements MyList<T> {
+public class MyArrayList<T extends Comparable<T>> {
     private Object[] data;
     private int size;
+    private static final int DEFAULT_CAPACITY = 10;
 
     public MyArrayList() {
-        data = new Object[10]; // Default initial capacity
+        data = new Object[DEFAULT_CAPACITY];
         size = 0;
     }
 
-    @Override
     public void add(T item) {
         ensureCapacity();
         data[size++] = item;
     }
 
-    @Override
-    public void add(int index, T item) {
-        if (index < 0 || index > size) throw new IndexOutOfBoundsException();
-        ensureCapacity();
-        for (int i = size; i > index; i--) {
-            data[i] = data[i - 1];
-        }
-        data[index] = item;
-        size++;
-    }
-
-    @Override
-    public void set(int index, T item) {
-        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
-        data[index] = item;
-    }
-
-    @Override
     public T get(int index) {
         if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
         return (T) data[index];
     }
 
-    @Override
-    public void remove(int index) {
+    public void set(int index, T item) {
         if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        data[index] = item;
+    }
+
+    public T remove(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        T removed = (T) data[index];
         for (int i = index; i < size - 1; i++) {
             data[i] = data[i + 1];
         }
         data[--size] = null;
+        return removed;
     }
 
-    @Override
-    public void clear() {
-        data = new Object[10];
-        size = 0;
-    }
-
-    @Override
     public int size() {
         return size;
+    }
+
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            data[i] = null;
+        }
+        size = 0;
     }
 
     private void ensureCapacity() {
@@ -75,95 +60,6 @@ public class MyArrayList<T extends Comparable<T>> implements MyList<T> {
             data = newData;
         }
     }
-
-    @Override
-    public T getFirst() {
-        return get(0);
-    }
-
-    @Override
-    public T getLast() {
-        return get(size - 1);
-    }
-
-    @Override
-    public void addFirst(T item) {
-        add(0, item);
-    }
-
-    @Override
-    public void addLast(T item) {
-        add(item);
-    }
-
-    @Override
-    public void removeFirst() {
-        remove(0);
-    }
-
-    @Override
-    public void removeLast() {
-        remove(size - 1);
-    }
-
-    @Override
-    public int indexOf(Object object) {
-        for (int i = 0; i < size; i++) {
-            if (data[i].equals(object)) return i;
-        }
-        return -1;
-    }
-
-    @Override
-    public int lastIndexOf(Object object) {
-        for (int i = size - 1; i >= 0; i--) {
-            if (data[i].equals(object)) return i;
-        }
-        return -1;
-    }
-
-    @Override
-    public boolean exists(Object object) {
-        return indexOf(object) != -1;
-    }
-
-    @Override
-    public Object[] toArray() {
-        Object[] result = new Object[size];
-        for (int i = 0; i < size; i++) {
-            result[i] = data[i];
-        }
-        return result;
-    }
-
-    @Override
-    public void sort() {
-        for (int i = 0; i < size - 1; i++) {
-            for (int j = 0; j < size - i - 1; j++) {
-                T a = (T) data[j];
-                T b = (T) data[j + 1];
-                if (a.compareTo(b) > 0) {
-                    Object temp = data[j];
-                    data[j] = data[j + 1];
-                    data[j + 1] = temp;
-                }
-            }
-        }
-    }
-
-    @Override
-    public Iterator<T> iterator() {
-        return new Iterator<T>() {
-            int cursor = 0;
-
-            public boolean hasNext() {
-                return cursor < size;
-            }
-
-            public T next() {
-                return (T) data[cursor++];
-            }
-        };
-    }
 }
+
 // ==== MyArrayList END ====
